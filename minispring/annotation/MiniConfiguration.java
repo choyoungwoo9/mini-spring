afc1650fbd856665085b0491d4a072a8f75568b1
조영우 <choyoungwoo9@naver.com>
2025-08-17 16:46:52 +0900

    chore: 경로 변경

--- minispring/annotation/MiniConfiguration.java
+++ minispring/annotation/MiniConfiguration.java
@@ -1,12 +1,11 @@
-package mini.annotation;
+package minispring.annotation;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface MiniBean {
-
+public @interface MiniConfiguration {
 }
